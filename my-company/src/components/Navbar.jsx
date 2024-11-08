07d564486c69cce8c3f6fe9340a6cee2ef624a29
Peggy Annan <pegannan.22@gmail.com>
2024-11-08 22:40:02 +0000

    final additions

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 function Home() {
     return (
         Link from './react-router-dom' to (Home.jsx, About.jsx, Services.jsx, Contact.jsx)
+        <p style={{backgroundColor:'black', display:'inline', justifyContent:'center'}}>Navigatiom</p>
     );
   }
 
