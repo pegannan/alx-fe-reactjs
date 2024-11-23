edc45aa8b35977a7569d826b899b32864c7790ab
Peggy Annan <pegannan.22@gmail.com>
2024-11-23 11:59:30 +0000

    new changes

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const RegistrationForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setEmail({''});
+    setPassword('');
+    setUsername('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <textarea
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+       <textarea
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+    </form>
+  );
+};
