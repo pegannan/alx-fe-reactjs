25397fe210c38d7454f71c9eda447a9043532b55
Peggy Annan <pegannan.22@gmail.com>
2024-11-24 12:33:39 +0000

    new

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -31,6 +31,21 @@ const ReactQuery= () => {
         </div>
     );
     <button>Catch Data</button>
+
+    import { render, screen, fireEvent } from '@testing-library/react';
+import Button from './Button';
+import {  jest } from '@jest/globals';
+
+// Test to check if button click triggers event handler
+test('calls onClick when button is clicked', () => {
+    const handleClick = jest.fn();
+    render(<Button onClick={handleClick}>Click me</Button>);
+    // Simulate click event
+    fireEvent.click(screen.getByText('Click me'));
+    // Verify if the click handler is called
+    expect(handleClick).toHaveBeenCalledTimes(1);
+});
+
 };
 
 
