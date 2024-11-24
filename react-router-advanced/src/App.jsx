ff442978d2ee9c205687a4bc2241c4169ef88029
Peggy Annan <pegannan.22@gmail.com>
2024-11-24 14:39:24 +0000

    new

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -4,8 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import { BrowserRouter as Router, Routes, Switch, Link, useRouteMatch } from 'react-router-dom';
+<BrowserRouter>
+  <Routes>
+      <Route path ="blog/:id => BlogPost"/>
+  </Routes>
 
-
+  
+</BrowserRouter>
 function App() {
   const [count, setCount] = useState(0)
 
