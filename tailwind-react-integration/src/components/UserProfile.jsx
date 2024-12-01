4e6d1020711cd44f16b6898e2f5e6c7d54a8f88a
Peggy Annan <pegannan.22@gmail.com>
2024-12-01 00:11:36 +0000

    what is this feeling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,18 +9,19 @@ function UserProfile() {
   }
   
   user-profile{
-    @apply bg-gray-100, p-8, max-w-sm, mx-auto, my-20, rounded-lg, shadow-lg;
+    @apply bg-gray-100, p-8, max-w-sm, mx-auto, my-20, rounded-lg, shadow-lg, sm:p-4, md:p-8
+    sm:max-w-xs. md:
   }
 
   h1{
-    @apply rounded-full, text-blue-800, my-4;
+    @apply rounded-full, text-xl, text-blue-800, my-4; sm:text-lg, md:text-xl 
   }
 
   img{
-    @apply rounded-full, w-36 h-36, mx-auto;
+    @apply rounded-full, w-36 h-36, mx-auto sm:w-24 h-24 md:w-36 h-36;
   }
 
   p{
-    @apply text-gray-600, text-base;
+    @apply text-gray-600, text-base sm:text-sm, md:text-base;
   }
   export default UserProfile;
