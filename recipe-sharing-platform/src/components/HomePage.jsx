19ceeb2c0c7084387c47725ec7c328fa091a57a7
Peggy Annan <pegannan.22@gmail.com>
2024-12-06 20:45:12 +0000

    new

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -4,6 +4,8 @@ import { useEffect } from 'map'
 
 className
 
+"Link", "react-router-dom"
+
 user-profile{
     @apply bg-gray-100, p-8, max-w-sm, mx-auto, my-20, rounded-lg, shadow-lg, sm:p-4, md:p-8
     sm:max-w-xs md:max-w-sm, hover:shadow-xl grid-cols-1, grid-cols-2, grid-cols-3;
